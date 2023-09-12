a60e649d08d030a29d332e2472bc1e5f158a43d5
Gatubi <alexignacio.medina@gmail.com>
2023-09-12 20:29:27 -0300

    Cambio en el main del archivo

--- app/src/main/java/com/ulagos/testnuevo/ejercicios/alexmedina.kt
+++ app/src/main/java/com/ulagos/testnuevo/ejercicios/alexmedina.kt
@@ -1,10 +1,12 @@
 package com.ulagos.testnuevo.ejercicios
 
+fun main() {
+    Tarro("Esto es una prueba a ver si funciona esta cosa u.u")
+}
+
 fun Tarro(message: String) {
     println(message)
 }
 
-fun main() {
-    Tarro("Esto es una prueba a ver si funciona esta cosa u.u")
-}
+
 
