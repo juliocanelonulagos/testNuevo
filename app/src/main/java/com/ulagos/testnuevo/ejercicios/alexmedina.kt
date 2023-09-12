7f8df45ea4ef505c5d7f655c63104a21a1e43265
Gatubi <alexignacio.medina@gmail.com>
2023-09-12 20:33:38 -0300

    Correccion posterior a la revisión

--- app/src/main/java/com/ulagos/testnuevo/ejercicios/alexmedina.kt
+++ app/src/main/java/com/ulagos/testnuevo/ejercicios/alexmedina.kt
@@ -1,12 +1,14 @@
 package com.ulagos.testnuevo.ejercicios
 
+fun Tarro(message: String) {
+    println(message)
+}
+
 fun main() {
     Tarro("Esto es una prueba a ver si funciona esta cosa u.u")
 }
 
-fun Tarro(message: String) {
-    println(message)
-}
+
 
 
 
